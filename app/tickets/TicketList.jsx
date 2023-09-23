7c69eebd406071cd750e1e9f6f28db50ba64a6f3
Anitasilvi <anitaferdina06@gmail.com>
2023-09-23 13:09:27 +0700

    dynamic segments

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 async function getTickets(){
     const res = await fetch("http://localhost:4000/tickets",{
         next: {
@@ -12,19 +14,21 @@ export default async function TicketsList(){
     const tickets = await getTickets()
 
     return(
-       <>
+    <>
         {tickets.map((ticket) => (
             <div key={ticket.id} className="card my-5">
-                <h3>{ticket.title}</h3>
-                <p>{ticket.body.slice(0, 200)}...</p>
-                <div className={`pill ${ticket.priority}`}>
-                    {ticket.priority} priority
-                </div>
+                <Link href={`/tickets/${ticket.id}`}>
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </Link>
             </div>
         ))}
         {tickets.length === 0 && (
             <p className="text-center">There are no open tickets</p>
         )}
-       </>
+    </>
     )
 }
